f9abcdd7336ed9194cb59bef063cd9a0a245a2cc
Manfredi Smaniotto <manfri96@live.it>
2018-04-26 16:45:23 +0200

    inserimento test verifica relazione tokenizer

---
+++ DeSpeect/Test/View_Test.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+#include "setspeectconfigcommand.h"
+#include "../Model/Command/header/commandlist.h"
+
+TEST(View, VerifyRelationTest){
+    Speect* s=new Speect();
+    CommandList::CommandBuilder* builder=new CommandList::CommandBuilder(s);
+    CommandList* commands;
+    commands=builder->LoadConfig(Configuration::Voice, "./cmu_arctic_slt/voice.json").getCommandList();
+    commands->executeAll();
+    std::string text="hi everybody";
+    commands=builder->LoadConfig(Configuration::UtteranceText, text).getCommandList();
+    commands->executeAll();
+    std::list<std::string> processorList;
+    processorList.push_back("Tokenize");
+    commands=builder->WithProcessors(processorList).getCommandList();
+    commands->executeAll();
+    EXPECT_EQ(commands->getRelationNames().front(), "Token");
+    delete s;
+    delete commands;
+}
